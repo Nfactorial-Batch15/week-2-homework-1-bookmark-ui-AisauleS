b0f08262afb167721e68109c00ad81016b8daf43
AisauleS <95014799+AisauleS@users.noreply.github.com>
2021-12-08 17:58:24 +0500

    Initial Commit

---
+++ Bookmark/BookmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookmarkApp.swift
+//  Bookmark
+//
+//  Created by Aisaule Sibatova on 08.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct BookmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AddBookmarkView()
+        }
+    }
+}
